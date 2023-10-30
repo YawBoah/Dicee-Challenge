e1dc1620c63c09ce04a46d638c10138ada8c8d6b
YBoah <yawboahene.yb@gmail.com>
2023-10-30 06:40:13 +0000

    Declaring of winners

--- index.js
+++ index.js
@@ -16,3 +16,14 @@ var randomNumber2 = Math.floor(Math.random()*6) + 1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//The Outcome results
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 won 🚩";
+}
+else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 won 🚩";
+}
+else {
+    document.querySelector("h1").innerHTML ="Draw🎌"
+}

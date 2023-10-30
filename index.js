f304341390e11c9f299358caba4619d4f990a691
YBoah <yawboahene.yb@gmail.com>
2023-10-30 06:33:22 +0000

    Randomizing of the right side dice

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Randomizing of the first imaage
+//Randomizing of the first or left imaage
 var randomNumber1 = Math.floor(Math.random()*6) + 1; //Randoming the numbers 1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; // Randoming the images
@@ -9,3 +9,10 @@ var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
+
+//Randomizing of the second or right image
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
